819bbbf2aed875475245fc77908f14bbb11ddb70
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2019-03-06 15:30:34 +0100

    Add data loader, failed covariance forecasting experiment and some more classic methods

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Environment setup
 Requires anaconda python distribution, such as: https://conda.io/miniconda.html
 ```
-conda create -n quant-invest python=3.7.1 jupyterlab numpy pandas scikit-learn pytorch matplotlib seaborn tqdm requests beautifulsoup4
+conda create -n quant-invest python=3.7.1 jupyterlab numpy pandas scikit-learn pytorch matplotlib seaborn tqdm requests beautifulsoup4 numba
 source activate quant-invest
 ```
---
+++ utils/data_loader.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+
+FUNDS_CSV = 'input/Quant_Invest_Fundusze.csv'
+STOCKS_CSV = 'input/all_indices_close.csv'
+COMMODITIES_CSV = 'input/all_commodities_close.csv'
+RATES_CSV = 'input/policy_rates.csv'
+FX_CSV = 'input/exchange_rates.csv'
+
+
+def _load_indexed(path, index_col='Daty', sep=',') -> pd.DataFrame:
+    df = pd.read_csv(path, index_col=index_col, sep=sep)
+    df.index = pd.to_datetime(df.index)
+    return df.sort_index()
+
+
+def load_funds() -> pd.DataFrame:
+    return _load_indexed(FUNDS_CSV, sep=';')
+
+
+def load_stocks() -> pd.DataFrame:
+    return _load_indexed(STOCKS_CSV, index_col='Data')
+
+
+def load_commodities() -> pd.DataFrame:
+    return _load_indexed(COMMODITIES_CSV, index_col='Data')
+
+
+def load_rates() -> pd.DataFrame:
+    policy_rates = pd.read_csv(RATES_CSV, index_col = 0)
+    policy_rates = policy_rates.pivot(index='date', columns='reference_area', values='obs_value')
+    policy_rates.index = pd.to_datetime(policy_rates.index)
+    return policy_rates.iloc[:(-42)]  # keep only years 2000-2018
+
+
+def load_fx() -> pd.DataFrame:
+    exchange_rates = pd.read_csv(FX_CSV, index_col=0)
+    exchange_rates.index = pd.to_datetime(exchange_rates['Date'])
+    shortnames={}
+    for col in exchange_rates.columns[1:]:
+        start = col.index('(')
+        end = col.index(')')
+        shortnames[col] = col[(start+1):end]
+    return exchange_rates.rename(columns = shortnames).drop(columns=['Date'])
+
+
+def load_all() -> pd.DataFrame:
+    return pd.concat([
+        load_funds(),
+        load_stocks(),
+        load_commodities(),
+        load_rates(),
+        load_fx()
+    ], axis='columns')
